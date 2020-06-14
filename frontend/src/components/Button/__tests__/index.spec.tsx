c835fd4aede5ccf52543d0c49920131482c30a8e
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-06-14 15:17:50 -0300

    test: confgiguration

--- frontend/src/components/Button/__tests__/index.spec.tsx
+++ frontend/src/components/Button/__tests__/index.spec.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react-native';
 
 import Button from '..';
+
+jest.mock(
+  'react-native/Libraries/Components/Touchable/TouchableOpacity.js',
+  () => {
+    const { TouchableHighlight } = require('react-native');
+    const MockTouchable = (props: any) => {
+      return <TouchableHighlight {...props} />;
+    };
+    MockTouchable.displayName = 'TouchableOpacity';
+
+    return MockTouchable;
+  },
+);
 
 describe('Button', () => {
   it('should be able to render button and props', () => {
     const props = {
-      onClick: jest.fn(),
+      onPress: jest.fn(),
     };
 
     const { getByText, getByTestId } = render(<Button {...props}>Test</Button>);
 
-    expect(getByTestId('button-text')).toContainElement(getByText('Test'));
+    expect(getByText('Test')).toBeTruthy();
 
-    fireEvent.click(getByTestId('button'));
+    fireEvent.press(getByTestId('button'));
 
-    expect(props.onClick).toHaveBeenCalled();
+    expect(props.onPress).toHaveBeenCalled();
   });
 });
