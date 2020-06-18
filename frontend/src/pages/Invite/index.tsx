670a51d412d3a44556e33f5f7cb813a51e59aada
Andre Coelho <andre.coelho@axur.com>
2020-06-18 15:38:22 -0300

    feat: copy to clipboard

--- frontend/src/pages/Invite/index.tsx
+++ frontend/src/pages/Invite/index.tsx
@@ -1,9 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { Clipboard } from 'react-native';
 import { useParams, useHistory } from 'react-router-dom';
+import { FiCopy } from 'react-icons/fi';
 
 import { useIntegration } from '../../hooks/integration';
 
-import { Container } from './styles';
+import {
+  Container,
+  LogoText,
+  Info,
+  LinkContainer,
+  Link,
+  CopyButton,
+} from './styles';
 
 interface Params {
   id: string;
@@ -28,7 +37,22 @@ const Invite: React.FC = () => {
     }
   }, [error, history]);
 
-  return <Container></Container>;
+  const copyToClipboard = useCallback(() => {
+    Clipboard.setString(`${process.env.REACT_APP_API}/player2/${id}`);
+  }, []);
+
+  return (
+    <Container>
+      <LogoText>Jogo da velha</LogoText>
+      <Info>Envie o link para alguem e aguarde a sua entreada</Info>
+      <LinkContainer>
+        <Link>{`${process.env.REACT_APP_API}/player2/${id}`}</Link>
+        <CopyButton onPress={copyToClipboard}>
+          <FiCopy size={15} color="#efefef" />
+        </CopyButton>
+      </LinkContainer>
+    </Container>
+  );
 };
 
 export default Invite;
--- frontend/src/pages/Invite/styles.ts
+++ frontend/src/pages/Invite/styles.ts
@@ -6,3 +6,41 @@ export const Container = styled.View`
   justify-content: center;
   height: 100vh;
 `;
+
+export const LogoText = styled.Text`
+  color: #efefef;
+  font-family: 'Permanent Marker';
+  font-size: 30px;
+  text-align: center;
+`;
+
+export const Info = styled.Text`
+  color: #efefef;
+  font-size: 14px;
+  text-align: center;
+  width: 200px;
+  margin: 20px 0;
+`;
+
+export const LinkContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Link = styled.Text`
+  color: #efefef;
+  font-size: 14px;
+`;
+
+export const CopyButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  height: 30px;
+  width: 30px;
+  justify-content: center;
+  align-items: center;
+  border-width: 1px;
+  border-radius: 15px;
+  border-color: #efefef;
+  margin-left: 10px;
+`;
